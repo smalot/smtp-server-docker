337c47356feb3858957a2a7d1a61d432f6c92da0
Sebastien Malot <smalot@actualys.com>
2017-01-09 01:59:21 +0100

    dev

--- server.php
+++ server.php
@@ -6,7 +6,6 @@ use App\Event\ServerSubscriber;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
-use Smalot\Smtp\Server\Event\LogSubscriber;
 use React\EventLoop\Factory;
 use Smalot\Smtp\Server\Server;
 
@@ -15,7 +14,6 @@ $dispatcher = new EventDispatcher();
 $stream = fopen('php://output', 'w');
 $handler = new StreamHandler($stream, Logger::INFO);
 $logger = new Logger('log', [$handler]);
-//$dispatcher->addSubscriber(new LogSubscriber($logger));
 $dispatcher->addSubscriber(new ServerSubscriber($logger, '/data'));
 
 $loop = Factory::create();
